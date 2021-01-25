cf127b16334f0b3cbdbe136e0dc941744fe20087
Md. Ashikun Nabi <ashikunnabituhin@gmail.com>
2021-01-25 13:57:25 +0600

    Updates

---
+++ precache-manifest.ed56ef539576559542164aa7da699e22.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feb9858cbea2891c10fde6fb5cc5e2a",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/index.html"
+  },
+  {
+    "revision": "383fdfbc014dee9fc831",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/css/2.2b5ba5df.chunk.css"
+  },
+  {
+    "revision": "b54f2163c919e49dda98",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/css/main.abd34197.chunk.css"
+  },
+  {
+    "revision": "383fdfbc014dee9fc831",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/js/2.0a8da5d2.chunk.js"
+  },
+  {
+    "revision": "bccd7ff86acbe080e6809a3a747ffb08",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/js/2.0a8da5d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54f2163c919e49dda98",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/js/main.7b3d16e4.chunk.js"
+  },
+  {
+    "revision": "dede2f9077e75aa97fd3",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/js/runtime-main.3edab707.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "ad9d698c89bf6f7bcde1dacd996902fe",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/background2.ad9d698c.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "e877b4c764092b52ed2d4f58239f1389",
+    "url": "/Ashikunnabi/ashikunnabi.github.io/static/media/personal_description_banner.e877b4c7.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b663905433addedc50b40ea19b469b7b.js"
+  "/Ashikunnabi/ashikunnabi.github.io/precache-manifest.ed56ef539576559542164aa7da699e22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ashikunnabi/ashikunnabi.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
